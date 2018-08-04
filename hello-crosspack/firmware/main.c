257ffeacc7a06cd0ee871379af625de181099b48
y² <yy@yuyuan.org>
2018-08-04 03:17:49 -0700

    using avrdude.conf built from official source

--- hello-crosspack/firmware/main.c
+++ hello-crosspack/firmware/main.c
@@ -7,10 +7,10 @@ int main(void)
 
     for(;;){
         PORTB = 0b00100000; /* Turn on first LED bit/pin in PORTB */
-        _delay_ms(1000);    /* wait */
+        _delay_ms(100);    /* wait */
 
         PORTB = 0b00000000; /* Turn off all B pins, including LED */
-        _delay_ms(1000);    /* wait */
+        _delay_ms(100);    /* wait */
     }
 
     return 0;               /* never reached */
