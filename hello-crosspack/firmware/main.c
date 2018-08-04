7fb5da15b6c42a25050aecb3c04b9994cc03c3d0
y² <yy@yuyuan.org>
2018-08-04 02:50:27 -0700

    make flash # OK: Arduino Uno R3 blink LED

--- hello-crosspack/firmware/main.c
+++ hello-crosspack/firmware/main.c
@@ -3,13 +3,15 @@
 
 int main(void)
 {
-    DDRD = 1 << 4;           /* make the LED pin an output */
+    DDRB |= (1 << PB5);
+
     for(;;){
-        char i;
-        for(i = 0; i < 10; i++){
-            _delay_ms(30);  /* max is 262.14 ms / F_CPU in MHz */
-        }
-        PORTD ^= 1 << 4;    /* toggle the LED */
+        PORTB = 0b00100000; /* Turn on first LED bit/pin in PORTB */
+        _delay_ms(1000);    /* wait */
+
+        PORTB = 0b00000000; /* Turn off all B pins, including LED */
+        _delay_ms(1000);    /* wait */
     }
+
     return 0;               /* never reached */
 }
