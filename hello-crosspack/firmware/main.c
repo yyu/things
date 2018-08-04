fdbb26ae50112453cd215c37f180347f604a1fb8
y² <yy@yuyuan.org>
2018-08-03 21:48:35 -0700

    crosspack$ make
    $ make
    avr-gcc -Wall -Os -DF_CPU=8000000 -mmcu=atmega8 -c main.c -o main.o
    avr-gcc -Wall -Os -DF_CPU=8000000 -mmcu=atmega8 -o main.elf main.o
    rm -f main.hex
    avr-objcopy -j .text -j .data -O ihex main.elf main.hex
    avr-size --format=avr --mcu=atmega8 main.elf
    AVR Memory Usage
    ----------------
    Device: atmega8
    
    Program:      98 bytes (1.2% Full)
    (.text + .data + .bootloader)
    
    Data:          0 bytes (0.0% Full)
    (.data + .bss + .noinit)

--- hello-crosspack/firmware/main.c
+++ hello-crosspack/firmware/main.c
@@ -1,16 +1,15 @@
-/* Name: main.c
- * Author: <insert your name here>
- * Copyright: <insert your copyright message here>
- * License: <insert your license reference here>
- */
-
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
-    /* insert your hardware initialization here */
+    DDRD = 1 << 4;           /* make the LED pin an output */
     for(;;){
-        /* insert your main loop code here */
+        char i;
+        for(i = 0; i < 10; i++){
+            _delay_ms(30);  /* max is 262.14 ms / F_CPU in MHz */
+        }
+        PORTD ^= 1 << 4;    /* toggle the LED */
     }
-    return 0;   /* never reached */
+    return 0;               /* never reached */
 }
