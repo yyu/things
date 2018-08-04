d09f02fc3e21d6575ab9966c9805206a7aa75a54
y² <yy@yuyuan.org>
2018-08-04 03:28:46 -0700

    [pio] reformatted uno-pio

--- uno-pio/src/main.cpp
+++ uno-pio/src/main.cpp
@@ -10,23 +10,21 @@
 #define LED_BUILTIN 13
 #endif
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+    // initialize LED digital pin as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+void loop() {
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);
 
-  // wait for a second
-  delay(1000);
+    // wait for a second
+    delay(1000);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+    // turn the LED off by making the voltage LOW
+    digitalWrite(LED_BUILTIN, LOW);
 
-   // wait for a second
-  delay(1000);
+    // wait for a second
+    delay(1000);
 }
