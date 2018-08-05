2c53cea6a825518a98c61eb0692f7eced5e14c91
y² <yy@yuyuan.org>
2018-08-04 22:05:53 -0700

    [uno-pio-eclipse-servo-ES08A] uno drives servo

--- uno-pio-eclipse/src/main.cpp
+++ uno-pio-eclipse/src/main.cpp
@@ -1,34 +1,62 @@
 /**
- * Blink
+ * servo
  *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
+ * https://www.arduino.cc/en/Tutorial/Knob
  */
-#include "Arduino.h"
+
+#include <Arduino.h>
+#include <Servo.h>
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
 
+Servo myservo;  // create servo object to control a servo
+
+constexpr int POT_PIN = 0; // analog pin used to connect the potentiometer
+constexpr int SERVO_PIN = 9;
+
+template<typename A, typename B, typename C, typename D, typename E>
+constexpr void SERIAL_PRINT_5(const A a, const B& b, const C& c, const D& d, const E& e) {
+	Serial.print(a);
+	Serial.print(b);
+	Serial.print(c);
+	Serial.print(d);
+	Serial.print(e);
+	Serial.println();
+}
+
 void setup() {
 	Serial.begin(9600);
 
-    // initialize LED digital pin as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
+
+    myservo.attach(SERVO_PIN);
 }
 
-void loop() {
-	Serial.println("--------");
-
+void blink_led() {
     // turn the LED on (HIGH is the voltage level)
     digitalWrite(LED_BUILTIN, HIGH);
 
     // wait for a second
-    delay(1000);
+    delay(500);
 
     // turn the LED off by making the voltage LOW
     digitalWrite(LED_BUILTIN, LOW);
 
     // wait for a second
-    delay(1000);
+    delay(500);
+}
+
+void loop() {
+    int pot_val = analogRead(POT_PIN); // reads the value of the potentiometer (value between 0 and 1023)
+    int servo_angle = map(pot_val, 0, 1023, 0, 180); // scale it to use it with the servo (value between 0 and 180)
+
+    SERIAL_PRINT_5("pot: ", pot_val, " => ", "angle: ", servo_angle);
+
+    myservo.write(servo_angle); // sets the servo position according to the scaled value
+
+    delay(200); // waits for the servo to get there
+
+    blink_led();
 }
