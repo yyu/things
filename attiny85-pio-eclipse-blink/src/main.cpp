280cc212ae93eb7be1728872f50263ee6537b380
y² <yy@yuyuan.org>
2018-08-07 00:07:49 -0700

    [attiny85] it blinks

--- attiny85-pio-eclipse-blink/src/main.cpp
+++ attiny85-pio-eclipse-blink/src/main.cpp
@@ -1,12 +1,26 @@
 #include "Arduino.h"
 
-#define LED_PIN 0
+/*
+ * Tiny AVR Programmer:
+ *     * https://www.sparkfun.com/products/11801
+ *     * https://github.com/sparkfun/Tiny-AVR-Programmer
+ *     * https://static.sparkfun.com/datasheets/Dev/AVR/Tiny_Programmer.pdf
+ *
+ * useful info:
+ *     * https://learn.sparkfun.com/tutorials/tiny-avr-programmer-hookup-guide/
+ *
+ * I2C on attiny85
+ *     * http://playground.arduino.cc/Code/USIi2c
+ */
+
+// Sparkfun Tiny AVR Programmer has an LED connected to PIN 0
+#define LED_PIN 1 // so let's use PIN 1 for testing
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH); delay(1000);
-    digitalWrite(LED_PIN, LOW);  delay(1000);
+    digitalWrite(LED_PIN, HIGH); delay(500);
+    digitalWrite(LED_PIN, LOW);  delay(500);
 }
