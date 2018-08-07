49e43c335484461bd84482f3165ae9a73b2c936d
y² <yy@yuyuan.org>
2018-08-06 23:25:22 -0700

    [attiny85] it should blink

---
+++ attiny85-pio-eclipse-blink/src/main.cpp
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+
+#define LED_PIN 0
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_PIN, HIGH); delay(1000);
+    digitalWrite(LED_PIN, LOW);  delay(1000);
+}
