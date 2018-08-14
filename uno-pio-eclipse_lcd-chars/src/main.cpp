647c8221bed1603c84461c376faf0a63065fd0dc
y² <yy@yuyuan.org>
2018-08-14 03:11:51 -0700

    [lcd chars] two garage doors

--- uno-pio-eclipse_lcd-chars/src/main.cpp
+++ uno-pio-eclipse_lcd-chars/src/main.cpp
@@ -2,20 +2,20 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-uint8_t c0[8]  = {
+uint8_t north_west[8]  = {
+    0b10000,
     0b11111,
     0b10000,
     0b10000,
     0b10000,
     0b10000,
     0b10000,
-    0b10000,
-    0b10000
+    0b00000
 };
 
-uint8_t c1[8]  = {
-    0b11111,
+uint8_t north[8]  = {
     0b00000,
+    0b11111,
     0b00000,
     0b00000,
     0b00000,
@@ -24,114 +24,211 @@ uint8_t c1[8]  = {
     0b00000
 };
 
-uint8_t c2[8]  = {
+uint8_t north_east[8]  = {
+    0b00001,
     0b11111,
     0b00001,
     0b00001,
     0b00001,
     0b00001,
     0b00001,
-    0b00001,
-    0b00001
+    0b00000
 };
 
-uint8_t c3[8]  = {
-    0b10000,
+uint8_t west_middle[8]  = {
     0b10000,
     0b10000,
     0b11111,
     0b10000,
     0b10000,
     0b10000,
+    0b10000,
+    0b11111
+};
+
+uint8_t west_middle_1[8]  = {
+    0b10000,
+    0b10000,
+    0b11111,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b11111
+};
+
+uint8_t east_middle[8]  = {
+    0b00001,
+    0b00001,
+    0b11111,
+    0b00001,
+    0b00001,
+    0b00001,
+    0b00001,
+    0b11111
+};
+
+uint8_t south_west[8]  = {
+    0b10000,
+    0b11111,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b11111,
     0b10000
 };
 
-uint8_t c4[8]  = {
+uint8_t south_east[8]  = {
     0b00001,
+    0b11111,
+    0b00001,
+    0b00001,
+    0b00001,
+    0b00001,
+    0b11111,
+    0b00001
+};
+
+uint8_t north_west_south_0[8]  = {
+    0b11111,
+    0b10000,
+    0b10111,
+    0b10111,
+    0b10111,
+    0b10000,
+    0b11111,
+    0b10000,
+};
+
+uint8_t north_west_south_1[8]  = {
+    0b10000,
+    0b10000,
+    0b11111,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b11111
+};
+
+uint8_t north_east_south_0[8]  = {
+    0b11111,
+    0b00001,
+    0b11101,
+    0b11101,
+    0b11101,
+    0b00001,
+    0b11111,
+    0b00001,
+};
+
+uint8_t north_east_south_1[8]  = {
     0b00001,
     0b00001,
     0b11111,
     0b00001,
     0b00001,
     0b00001,
-    0b00001
-};
-
-uint8_t c5[8]  = {
-    0b10000,
-    0b10000,
-    0b10000,
-    0b10000,
-    0b10000,
-    0b10000,
-    0b10000,
-    0b11111
-};
-
-uint8_t c6[8]  = {
-    0b00000,
-    0b00000,
-    0b00000,
-    0b00000,
-    0b00000,
-    0b00000,
-    0b00000,
-    0b11111
-};
-
-uint8_t c7[8]  = {
-    0b00001,
-    0b00001,
-    0b00001,
-    0b00001,
-    0b00001,
-    0b00001,
     0b00001,
     0b11111
 };
+
+uint8_t north_south_0[8]  = {
+    0b11111,
+    0b00000,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b00000,
+    0b11111,
+    0b00000,
+};
+
+uint8_t north_south_1[8]  = {
+    0b00000,
+    0b00000,
+    0b11111,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b11111
+};
+
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-void setup()
-{
-  lcd.init();
-  lcd.backlight();
+int i = 0;
+int j = 0;
 
-    lcd.createChar(0, c0);
-    lcd.createChar(1, c1);
-    lcd.createChar(2, c2);
-    lcd.createChar(3, c3);
-    lcd.createChar(4, c4);
-    lcd.createChar(5, c5);
-    lcd.createChar(6, c6);
-    lcd.createChar(7, c7);
-    lcd.home();
+void move_to(int row, int col) {
+    i = row;
+    j = col;
+}
 
-    lcd.print("Hello world...");
+void show(int row, int col, uint8_t* matrix, int char_index) {
+    lcd.createChar(char_index, matrix);
+    lcd.setCursor(col, row);
+    lcd.write(char_index);
+}
 
-    lcd.setCursor(0, 1);
+void show(uint8_t* matrix, int char_index) {
+    show(i, j++, matrix, char_index);
+}
 
-    lcd.write(byte(0));
-    for (int i = 0; i < 10; i++) {
-        lcd.write(byte(1));
-    }
-    lcd.write(byte(2));
+void show_gate(int x, int y) {
+    move_to(x, y);
 
-    lcd.setCursor(0, 2);
+    show(north_west_south_0, 0);
+    show(north_south_0, 1);
+    show(north_south_0, 1);
+    show(north_east_south_0, 2);
+    show(north_west_south_0, 0);
+    show(north_south_0, 1);
+    show(north_south_0, 1);
+    show(north_east_south_0, 2);
 
-    lcd.write(byte(3));
-    for (int i = 0; i < 10; i++) {
-        lcd.write('-');
-    }
-    lcd.write(byte(4));
+    move_to(x + 1, y);
 
-    lcd.setCursor(0, 3);
+    show(west_middle, 3);
+    show(north_south_1, 7);
+    show(north_south_1, 7);
+    show(east_middle, 4);
+    show(west_middle, 3);
+    show(north_south_1, 7);
+    show(north_south_1, 7);
+    show(east_middle, 4);
 
-    lcd.write(byte(5));
-    for (int i = 0; i < 10; i++) {
-        lcd.write(byte(6));
-    }
-    lcd.write(byte(7));
+    move_to(x + 2, y);
+
+    show(west_middle, 3);
+    show(north_south_1, 7);
+    show(north_south_1, 7);
+    show(east_middle, 4);
+    show(west_middle, 3);
+    show(north_south_1, 7);
+    show(north_south_1, 7);
+    show(east_middle, 4);
+
+    move_to(x + 3, y);
+
+    show(north_west_south_1, 5);
+    show(north_south_1, 7);
+    show(north_south_1, 7);
+    show(north_east_south_1, 6);
+    show(north_west_south_1, 5);
+    show(north_south_1, 7);
+    show(north_south_1, 7);
+    show(north_east_south_1, 6);
+}
+
+void setup() {
+    lcd.init();
+    lcd.backlight();
+
+    show_gate(0, 0);
+    show_gate(0, 10);
 }
 
 void loop() {
