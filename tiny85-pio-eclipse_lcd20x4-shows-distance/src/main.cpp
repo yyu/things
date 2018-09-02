fa12f6554113698d35b207bc3261a5d52f803414
y² <yy@yuyuan.org>
2018-09-01 22:31:18 -0700

    [ATTiny85 distance@lcd] using perfboard

--- tiny85-pio-eclipse_lcd20x4-shows-distance/src/main.cpp
+++ tiny85-pio-eclipse_lcd20x4-shows-distance/src/main.cpp
@@ -3,8 +3,8 @@
 #include "LiquidCrystal_PCF8574_ATtiny85.h"
 
 // Ultrasonic Sensor HC-SR04
-const int trig_pin = 4;
-const int echo_pin = 3;
+const int trig_pin = 3;
+const int echo_pin = 4;
 
 // LCD
 #define I2C_ADDRESS 0x27
@@ -25,7 +25,7 @@ void setup() {
     lcd.print("Distance:");
 }
 
-int read_distance_in_cm() {
+void get_and_display_distance_in_cm() {
     // Clears the trig_pin
     digitalWrite(trig_pin, LOW);
     delayMicroseconds(2);
@@ -38,23 +38,17 @@ int read_distance_in_cm() {
     // Reads the echo_pin, returns the sound wave travel time in microseconds
     long duration = pulseIn(echo_pin, HIGH);
 
+    lcd.setCursor(0, 2);
+    lcd.print("duration: " + String(duration));
+
     // Calculating the distance in cm
     int distance = duration * 0.034 / 2;
 
-    return distance;
+    lcd.setCursor(0, 3);
+    lcd.print(String(distance) + " cm        ");
 }
-
-void lcd_show_lines(const String& s0, const String& s1) {
-    lcd.setCursor(0, 1);
-    lcd.print(s0);
-
-    lcd.setCursor(0, 2);
-    lcd.print(s1);
-}
-
 
 void loop() {
-    int dist = read_distance_in_cm();
-
-    lcd_show_lines("----------", String(dist) + "cm        ");
+    get_and_display_distance_in_cm();
+    delay(1000);
 }
