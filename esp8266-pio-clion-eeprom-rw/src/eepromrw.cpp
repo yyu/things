e23c25a359f8c2376b82478f9d38c55e8e1e2db2
y² <forresty@amazon.com>
2018-09-16 11:03:28 -0700

    [esp8266 eeprom] it writes a string to eeprom

--- esp8266-pio-clion-eeprom-rw/src/eepromrw.cpp
+++ esp8266-pio-clion-eeprom-rw/src/eepromrw.cpp
@@ -1,19 +1,17 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 
-const uint BLINK_HZ = 5;
-const uint BLINK_DELAY = 1000 / BLINK_HZ / 2;
-
 const uint EEPROM_SIZE = 512;
 
-void blink(uint sec) {
-    uint n = BLINK_HZ * sec;
+void blink(uint sec, uint hz) {
+    uint n = hz * sec;
+    uint blink_delay = 1000 / hz / 2;
 
     for (uint i = 0; i < n; i++) {
         digitalWrite(LED_BUILTIN, LOW);   // LOW means LED is on because it is active low on the ESP-01
-        delay(BLINK_DELAY);
+        delay(blink_delay);
         digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-        delay(BLINK_DELAY);
+        delay(blink_delay);
     }
 }
 
@@ -24,20 +22,20 @@ void eeprom_read_all(byte buf[]) {
         buf[i] = EEPROM.read(i);
     }
 
-    blink(1);  // blink on finish
-
     EEPROM.end();
+
+    blink(1, 5);  // blink on finish
 }
-void eeprom_write_all(byte buf[]) {
+void eeprom_write_all(byte buf[], size_t n) {
     EEPROM.begin(EEPROM_SIZE);
 
-    for (uint i = 0; i < EEPROM_SIZE; i++) {
+    for (uint i = 0; i < n; i++) {
         EEPROM.write(i, buf[i]);
     }
 
-    blink(3);  // blink on finish
+    EEPROM.commit();
 
-    EEPROM.end();
+    blink(5, 1);  // blink on finish
 }
 
 void serial_print_eeprom_all() {
@@ -55,6 +53,7 @@ void serial_print_eeprom_all() {
     eeprom_all[EEPROM_SIZE] = '\0';
     Serial.print((char*)eeprom_all);  // what's is? 78b4e04d81022324f932273ce2847e44
 
+    Serial.println();
     Serial.println("==========");
 }
 
@@ -64,6 +63,19 @@ void setup() {
     Serial.begin(115200);
 
     pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+    serial_print_eeprom_all();
+
+    delay(1000);
+
+    const char * s = "SSID_AS_AP= "
+                     "PASSWORD_AS_AP= "
+                     "SSID= "
+                     "PASSWORD= "
+                     "HUE_AUTH=";
+    eeprom_write_all((byte*)s, strlen(s) + 1);
+
+    delay(1000);
 
     serial_print_eeprom_all();
 }
