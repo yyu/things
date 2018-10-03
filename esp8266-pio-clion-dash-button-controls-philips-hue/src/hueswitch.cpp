7d85d18637e9334c167cc97506e0dc77f61331ac
y² <yy@yuyuan.org>
2018-10-03 02:01:36 -0700

    [esp8266 eeprom] wip

--- esp8266-pio-clion-dash-button-controls-philips-hue/src/hueswitch.cpp
+++ esp8266-pio-clion-dash-button-controls-philips-hue/src/hueswitch.cpp
@@ -1,28 +1,113 @@
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266HTTPClient.h>
+#include <EEPROM.h>
 
-const char * ap = "*";
-const char * ssid = "*";
-const char * password = "*";
-const char * HUE_AUTH = "*";
+const static uint EEPROM_SIZE = 512;
 
+class Parameters {
+public:
+    void load() {
+        _read_eeprom(_eeprom_all);
+        _populate_params(_eeprom_all);
+    }
+
+    char * get_ssid_as_ap() { return _ssid_as_ap; }
+    char * get_pwd_as_ap() { return _pwd_as_ap; }
+    char * get_ssid() { return _ssid; }
+    char * get_pwd() { return _pwd; }
+    char * get_hue_auth() { return _hue_auth; }
+
+private:
+    char * _read_eeprom(char * eeprom_all) {
+        memset(eeprom_all, 0, EEPROM_SIZE);
+
+        EEPROM.begin(EEPROM_SIZE);
+        for (uint i = 0; i < EEPROM_SIZE; i++) {
+            eeprom_all[i] = EEPROM.read(i);
+        }
+        EEPROM.end();
+
+        return eeprom_all;
+    }
+
+    void _populate_params(char * eeprom_all) {
+        char * p = eeprom_all;
+
+        while (*p) {
+            char * key = p;
+            char * eq = _find_char(p, '=');
+            *eq = 0;
+
+            char * value = eq + 1;
+            char * spc = _find_char(value, ' ');
+            *spc = 0;
+
+            p = spc + 1;
+
+            if (strcmp(key, "SSID_AS_AP") == 0) {
+                _ssid_as_ap = value;
+            } else if (strcmp(key, "PASSWORD_AS_AP") == 0) {
+                _pwd_as_ap = value;
+            } else if (strcmp(key, "SSID") == 0){
+                _ssid = value;
+            } else if (strcmp(key, "PASSWORD") == 0) {
+                _pwd = value;
+            } else if (strcmp(key, "HUE_AUTH") == 0) {
+                _hue_auth = value;
+            }
+
+            Serial.print(key);
+            Serial.print(": ");
+            Serial.print(value);
+            Serial.println();
+        }
+    }
+
+    char * _find_char(char * s, char c) {
+        char * p = s;
+        for (; *p; p++) {
+            if (*p == c) {
+                return p;
+            }
+        }
+        return p;
+    }
+
+    char _eeprom_all[EEPROM_SIZE];
+    char * _ssid_as_ap = 0;
+    char * _pwd_as_ap = 0;
+    char * _ssid = 0;
+    char * _pwd = 0;
+    char * _hue_auth = 0;
+};
+
+Parameters params;
 const byte DNS_PORT = 53;
-IPAddress apIP(192, 168, 4, 1);
-DNSServer dnsServer;
-
-// buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char ReplyBuffer[] = "acknowledged";       // a string to send back
-
+IPAddress ap_ip(192, 168, 4, 1);
+DNSServer dns_server;
 WiFiUDP udp;
-
 bool status = true; // for hue light bulb
 unsigned long timestamp = 0;
 
-void initWiFi() {
+void init_wifi() {
+    char * ap = params.get_ssid_as_ap();
+    char * password_as_ap = params.get_pwd_as_ap();
+    char * ssid = params.get_ssid();
+    char * password = params.get_pwd();
+
+    Serial.println();
+
+    Serial.println(ap);
+    Serial.println(password_as_ap);
+    Serial.println(ssid);
+    Serial.println(password);
+
+    Serial.println("AP");
     WiFi.mode(WIFI_AP_STA);
-    WiFi.softAP(ap);
+    WiFi.softAP(ap, password_as_ap, /* channel */ 1, /* hide ssid */ 0, /* max_connection */ 4);
+
+    Serial.println("STATION");
     WiFi.begin(ssid, password);
 
     IPAddress myIP = WiFi.softAPIP();
@@ -34,36 +119,71 @@ void initWiFi() {
         Serial.print(".");
     }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
+    Serial.print("WiFi connected. ");
+    Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
 }
 
-void initDNSServer() {
+void init_dns_server() {
     // modify TTL associated  with the domain name (in seconds)
     // default is 60 seconds
-    dnsServer.setTTL(300);
+    dns_server.setTTL(300);
 
     // set which return code will be used for all other domains (e.g. sending ServerFailure
     // instead of NonExistentDomain will reduce number of queries sent by clients)
     // default is DNSReplyCode::NonExistentDomain
-    dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+    dns_server.setErrorReplyCode(DNSReplyCode::ServerFailure);
 
     // start DNS server for a specific domain name
-    dnsServer.start(DNS_PORT, "0.amazon.pool.ntp.org", apIP);
+    dns_server.start(DNS_PORT, "0.amazon.pool.ntp.org", ap_ip);
 }
 
-void initUDP() {
+void init_udp() {
     udp.begin(123);
 
     Serial.println("UDP server started");
 }
 
+template <typename T>
+std::string join_array(const char * format, T a, char sep, size_t n) {
+    std::string s;
+    char num_buf[16];
+    for (int k = 0; k < n; k++) {
+        sprintf(num_buf, format, a[k]);
+        s += num_buf;
+        if (k != n - 1) {
+            s += sep;
+        }
+    }
+    return s;
+}
+
+void showClients()
+{
+    struct station_info * stat_info = wifi_softap_get_station_info();
+    uint8_t client_count = wifi_softap_get_station_num();
+
+    String str = "Number of clients = " + String(client_count) + "\n";
+    while (stat_info) {
+        std::string mac_addr = join_array("%0X", stat_info->bssid, ':', 6);
+        str += mac_addr.c_str();
+        str += " ";
+        IPAddress ip(stat_info->ip.addr);
+        std::string ip_addr = join_array("%0d", ip, '.', 4);
+        str += ip_addr.c_str();
+
+        stat_info = STAILQ_NEXT(stat_info, next);
+    }
+
+    Serial.println(str);
+}
+
 void switch_hue() {
+    char * hue_auth = params.get_hue_auth();
+
     unsigned long now = millis();
 
-    if (now - timestamp < 1000) {
+    if (now - timestamp < 10000) {
         return;
     }
 
@@ -72,75 +192,44 @@ void switch_hue() {
     HTTPClient http;
 
     String url_prefix = "http://192.168.10.110/api/";
-    String url = url_prefix + HUE_AUTH + "/lights/2/state";
+    String url = url_prefix + hue_auth + "/lights/2/state";
 
-    http.begin(url);
+    Serial.println(url);
+
+    if (!http.begin(url)) {
+        Serial.println("failed to begin http");
+        return;
+    }
     http.addHeader("Content-Type", "text/plain");
 
+    Serial.println("before HTTP PUT");
     int httpCode;
     if (status) {
         httpCode = http.PUT("{\"on\":false}");
-        status = false;
     } else {
         httpCode = http.PUT("{\"on\":true}");
-        status = true;
     }
+    if (httpCode == 200) {
+        status = !status;
+        showClients();
+    }
+    Serial.println(" after HTTP PUT");
+    Serial.println(httpCode);
 
     String response = http.getString();
 
-    Serial.println(httpCode);
     Serial.println(response);
 
     http.end();
 }
 
-void showClients()
-{
-    struct station_info *stat_info;
-    stat_info = wifi_softap_get_station_info();
-    uint8_t client_count = wifi_softap_get_station_num();
-    String str = "Number of clients = ";
-    str += String(client_count);
-    str += "\r\nList of clients : \r\n";
-    int i = 1;
-    while (stat_info) {
-        str += "Station #";
-        str += String(i);
-        str += " : ";
-        str += String(stat_info->bssid[0], HEX);
-        str += ":";
-        str += String(stat_info->bssid[1], HEX);
-        str += ":";
-        str += String(stat_info->bssid[2], HEX);
-        str += ":";
-        str += String(stat_info->bssid[3], HEX);
-        str += ":";
-        str += String(stat_info->bssid[4], HEX);
-        str += ":";
-        str += String(stat_info->bssid[5], HEX);
-        str += "\r\n";
-        i++;
-        stat_info = STAILQ_NEXT(stat_info, next);
-//    stat_info = stat_info->next;
-    }
-    Serial.println(str);
-}
+// buffers for receiving and sending data
+char packet_buffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+char reply_buffer[] = "acknowledged";       // a string to send back
 
-void setup() {
-    delay(10000); // so that I have plenty of time to open serial monitor
+bool should_switch_hue = false;
 
-    Serial.begin(115200);
-    Serial.println();
-    Serial.print("Configuring access point...");
-
-    initWiFi();
-    initDNSServer();
-    initUDP();
-}
-
-void loop() {
-    dnsServer.processNextRequest();
-
+void handle_ntp_request() {
     // if there's data available, read a packet
     int packetSize = udp.parsePacket();
     if (packetSize) {
@@ -157,18 +246,42 @@ void loop() {
         Serial.print(", port ");
         Serial.println(udp.remotePort());
 
+        should_switch_hue = true;
+
         // read the packet into packetBufffer
-        udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+        udp.read(packet_buffer, UDP_TX_PACKET_MAX_SIZE);
         Serial.println("Contents:");
-        Serial.println(packetBuffer);
+        Serial.println(packet_buffer);
 
         // send a reply, to the IP address and port that sent us the packet we received
         udp.beginPacket(udp.remoteIP(), udp.remotePort());
-        udp.write(ReplyBuffer);
+        udp.write(reply_buffer);
         udp.endPacket();
+    }
+}
 
+void setup() {
+    delay(10000); // so that I have plenty of time to open serial monitor
+
+    Serial.begin(115200);
+
+    params.load();
+
+    init_wifi();
+    init_dns_server();
+    init_udp();
+}
+
+void loop() {
+    should_switch_hue = false;
+
+//    switch_hue();
+    dns_server.processNextRequest();
+
+    handle_ntp_request();
+
+    if (should_switch_hue) {
         switch_hue();
-        showClients();
     }
 
     delay(100);
