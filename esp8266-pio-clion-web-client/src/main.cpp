87e83447124c7b27c9f15c155c13fb8c3d2ed82a
y² <yy@yuyuan.org>
2019-03-22 13:46:48 -0700

    [esp8266 web client] it connects wifi

--- esp8266-pio-clion-web-client/src/main.cpp
+++ esp8266-pio-clion-web-client/src/main.cpp
@@ -1,26 +1,36 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
 
-/*
-  ESP8266 Blink by Simon Peter
-  Blink the blue LED on the ESP-01 module
-  This example code is in the public domain
+void initWiFi() {
+  const char * ssid = "Guest";
+  const char * password = "";
 
-  The blue LED on the ESP-01 module is connected to GPIO1
-  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+  Serial.println();
+  Serial.println(ssid);
+  Serial.println(password);
 
-  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
-*/
+  Serial.println("STATION");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("WiFi connected. ");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because
-                                    // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+void setup()
+{
+  delay(3000);
+  Serial.begin(115200);
+
+  initWiFi();
+}
+
+void loop()
+{
+  delay(1000);
 }
