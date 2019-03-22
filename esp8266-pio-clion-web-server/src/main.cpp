21e204c595370192ba4a7ed68c8bc4f0456bb8b1
y² <yy@yuyuan.org>
2019-03-22 13:38:11 -0700

    [esp8266 web server] HTTP requests drive servo

--- esp8266-pio-clion-web-server/src/main.cpp
+++ esp8266-pio-clion-web-server/src/main.cpp
@@ -1,26 +1,138 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Servo.h>
 
-/*
-  ESP8266 Blink by Simon Peter
-  Blink the blue LED on the ESP-01 module
-  This example code is in the public domain
+const int servopin = 15;
 
-  The blue LED on the ESP-01 module is connected to GPIO1
-  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+WiFiServer server(8888);
+Servo blueservo;
 
-  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
-*/
+void initWiFi() {
+    const char * ap = "Guest8266";
+    const char * password_as_ap = "";
+//    const char * ssid = "N2G";
+//    const char * password = "";
+    const char * ssid = "Guest";
+    const char * password = "";
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+    Serial.println();
+
+    Serial.println(ap);
+    Serial.println(password_as_ap);
+    Serial.println(ssid);
+    Serial.println(password);
+
+    Serial.println("AP");
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP(ap, password_as_ap, /* channel */ 1, /* hide ssid */ 0, /* max_connection */ 4);
+
+    Serial.println("STATION");
+//    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.print("WiFi connected. ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because
-                                    // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+void setup()
+{
+    delay(3000);
+    Serial.begin(115200);
+    Serial.println();
+    Serial.print("Configuring access point...");
+
+    initWiFi();
+
+    server.begin();
+    Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
+
+    blueservo.attach(servopin);
+    Serial.printf("angle: %d \n", blueservo.read());
+}
+
+
+// prepare a web page to be send to a client (web browser)
+String prepareHtmlPage()
+{
+  String htmlPage =
+          String("HTTP/1.1 200 OK\r\n") +
+          "Content-Type: text/html\r\n" +
+          "Connection: close\r\n" +  // the connection will be closed after completion of the response
+          "Refresh: 5\r\n" +  // refresh the page automatically every 5 sec
+          "\r\n" +
+          "<!DOCTYPE HTML>" +
+          "<html>" +
+          "Analog input:  " + String(analogRead(A0)) +
+          "</html>" +
+          "\r\n";
+  return htmlPage;
+}
+
+void servogo(int a) {
+    if (!blueservo.attached()) {
+        blueservo.attach(servopin);
+    }
+    blueservo.write(a);
+    delay(1000);
+//    blueservo.detach();
+}
+
+void loop()
+{
+    WiFiClient client = server.available();
+    // wait for a client (web browser) to connect
+    if (client)
+    {
+        Serial.println("\n[Client connected]");
+        while (client.connected())
+        {
+            // read line by line what the client (web browser) is requesting
+            if (client.available())
+            {
+                String line = client.readStringUntil('\r');
+                Serial.print(line);
+                // wait for end of client's request, that is marked with an empty line
+                if (line.length() == 1 && line[0] == '\n')
+                {
+                    Serial.println("printing html");
+                    client.println(prepareHtmlPage());
+                    break;
+                }
+
+                int k = line.indexOf("angle");
+                Serial.printf("<k:%d>\n", k);
+
+                if (k != -1) {
+                    int endidx = line.indexOf("degree");
+                    int a = line.substring(k + 5, endidx).toInt();
+                    Serial.printf("<a:%d>\n", a);
+                    servogo(a);
+                } else if (line.indexOf("north") != -1) {
+                    servogo(0);
+                } else if (line.indexOf("south") != -1) {
+                    servogo(180);
+                } else if (line.indexOf("center") != -1) {
+                    servogo(90);
+                } else if (line.indexOf("stop") != -1) {
+                    if (blueservo.attached()) {
+                        blueservo.detach();
+                    }
+                }
+            }
+        }
+        delay(1); // give the web browser time to receive the data
+
+        // close the connection:
+        client.stop();
+        Serial.println("[Client disconnected]");
+    }
 }
